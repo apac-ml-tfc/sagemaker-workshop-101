0468263ce364fa037e3c886a1db381079e1b90bf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 07:51:45 +0000

    Adding tarfile member sanitization to extractall()

--- custom_tensorflow_keras_nlp/util/preprocessing.py
+++ custom_tensorflow_keras_nlp/util/preprocessing.py
@@ -27,7 +27,26 @@ def download_dataset():
 
     with tarfile.open("data/ag_news_csv.tgz", 'r:gz') as tar:
         print("Unzipping...")
-        tar.extractall(path="data/")
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path="data/")
         tar.close()
     try:
         # Clean up the noise in the folder, don't care too much if it fails:
